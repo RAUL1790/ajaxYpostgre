aedff3e099990520ad3da706064565098c25cfe7
Raul <amos.1790@yahoo.com>
2014-12-22 20:11:59 -0430

    todo

---
+++ ejer1.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	
+</body>
+<script type="text/javascript" src="js/jquery-2.1.1.min.js"></script>
+<script type="text/javascript" src="js/funciones.js"></script>
+</html>
